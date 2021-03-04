3400ce30c1063c63cbee3511a6ef775ee51c3399
shun0305 <shunbbra@icloud.com>
2021-03-04 22:06:17 +0900

    orderscreen

--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -10,6 +10,7 @@ export default (state = initialState, action) => {
         case ADD_ORDER:
             const newOrder = new Order(
                 new Date().toString(),
+                action.orderData.items,
                 action.orderData.amount,
                 new Date()
             );
